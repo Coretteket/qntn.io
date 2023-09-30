45a04225c36da6f3b73ef0dc0d1cbb872eae4ccb
Quinten Coret <Qcoret@gmail.com>
2023-09-30 21:50:48 +0200

    Solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), solid()],
   adapter: vercel(),
-  output: "server",
+  output: "server"
 });
