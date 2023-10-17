c8c986c8514d1060e2f7623a16b5cbb16b513bf7
Quinten Coret <Qcoret@gmail.com>
2023-10-17 11:58:23 +0200

    simplify [slug].ts

--- src/pages/[slug].ts
+++ src/pages/[slug].ts
@@ -2,11 +2,7 @@ import { kv } from "@/lib/kv";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ params }) => {
-  const slug = params.slug as string;
-
-  const url = await kv.hget("url_map", slug);
-
+  const url = await kv.hget("url_map", params.slug!);
   if (!url) return new Response("Not found", { status: 404 });
-
   return Response.redirect(url as string);
 };
