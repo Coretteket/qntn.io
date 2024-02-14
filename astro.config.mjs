94c68aee6cec990eedaa881e1d30e2141d849661
Quinten Coret <Qcoret@gmail.com>
2024-02-14 10:15:01 +0100

    isr test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,12 @@ import solid from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solid()],
-  adapter: vercel(),
+  adapter: vercel({
+    isr: {
+      expiration: 60 * 60 * 24 * 31, // 31 days
+      bypassToken: import.meta.env.BYPASS_TOKEN,
+      exclude: ['/slugs', '/slugs/add', '/slugs/edit', '/slugs/login', '/slugs/logout', '/slugs/remove']
+    }
+  }),
   output: "server"
 });
