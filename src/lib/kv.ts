35b15e59c3221880b2041c816812b3e1e4918bae
Coretteket <33397397+Coretteket@users.noreply.github.com>
2025-04-28 11:36:00 +0000

    env

--- src/lib/kv.ts
+++ src/lib/kv.ts
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 
-export const kv = createClient();
+export const kv = createClient({ url: "redis://redis:6379" });
 
 kv.on("error", (err) => console.log("Redis Client Error", err));
 
