faf6acc2c003a1307516d544da2bc4a61e06d490
coretteket <33397397+Coretteket@users.noreply.github.com>
2025-10-06 20:15:22 +0200

    up deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
-import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
-  integrations: [solid()],
   adapter: node({ mode: "standalone" }),
   output: "server",
 });
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ssh hetzner << 'EOF'
-cd qntn.io
-git pull
-docker compose up -d --build
-exit
-EOF
