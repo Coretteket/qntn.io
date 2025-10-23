71cb7a75259bae305e3cc98d5e75c81e603f1bb5
coretteket <33397397+Coretteket@users.noreply.github.com>
2025-10-23 17:37:23 +0200

    fix language detection

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,9 @@
 import type { APIContext } from "astro";
 
-export async function GET({ preferredLocale }: APIContext) {
-  const language = preferredLocale?.includes("nl") ? "nl" : "en";
-  const response = await fetch("http://localhost:4321/" + language);
+export async function GET({ request }: APIContext) {
+  const acceptLanguage = request.headers.get("Accept-Language") ?? "nl";
+  const locale = acceptLanguage.includes("nl") ? "nl" : "en";
+  const response = await fetch("http://localhost:4321/" + locale);
   const body = await response.text();
   return new Response(body, response);
 }
